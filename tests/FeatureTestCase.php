9855f217bc5e9be65c399706603ebe5e2b1782da
jparradog <parradojohn1421727@gmail.com>
2019-04-18 18:32:35 -0500

    Add feature test case

---
+++ tests/FeatureTestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * summary
+ */
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class FeatureTestCase extends TestCase
+{
+    use DatabaseTransactions;
+    public function __construct()
+    {
+
+    }
+}
